e4f53db069c961312c52e29779a4b3ee56248866
iamlarrybob <akinbobola.lanre@gmail.com>
2026-03-12 05:39:55 -0700

    import next response

--- app/BackEnd/api/userExist/route.js
+++ app/BackEnd/api/userExist/route.js
@@ -1,17 +1,44 @@
+// import { NextResponse } from "next/server";
+// import user from "../../models/user";
+// import { connectDB } from "../../utils/Database";
+
+// export async function POST(req) {
+//     try {
+//         await connectDB();
+//         const { email } = await req.json();
+//         const User = await user.findOne({ email }).select("_id");
+//         console.log("user:", User);
+//         return NextResponse.json({ User })
+
+//     } catch (err) {
+//         console.log(err);
+
+//     }
+// }
+
+
 import { NextResponse } from "next/server";
 import user from "../../models/user";
 import { connectDB } from "../../utils/Database";
 
 export async function POST(req) {
-    try {
-        await connectDB();
-        const { email } = await req.json();
-        const User = await user.findOne({ email }).select("_id");
-        console.log("user:", User);
-        return NextResponse.json({ User })
+  try {
+    await connectDB();
 
-    } catch (err) {
-        console.log(err);
+    const { email } = await req.json();
 
-    }
+    const User = await user.findOne({ email }).select("_id");
+
+    console.log("user:", User);
+
+    return NextResponse.json({ User });
+
+  } catch (err) {
+    console.log(err);
+
+    return NextResponse.json(
+      { message: "Server Error" },
+      { status: 500 }
+    );
+  }
 }
